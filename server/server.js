eb329fcd3ff18b6ce49fe73955f1cb443f35dc3d
Nishant Singh Sirohi <91972779+nishantsir0hi@users.noreply.github.com>
2025-04-30 15:40:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ if (!fs.existsSync(path.join(__dirname, 'uploads'))) {
 
 // Middleware
 app.use(cors({
-  origin: ['https://streamify-2.vercel.app', 'http://localhost:5173'],
+  origin: ['https://streamify-xi-blue.vercel.app', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Range'],
